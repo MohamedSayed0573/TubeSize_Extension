803ea66eb54228933f99f3b4e4e4b1f71732096f
Mohamed <20412023101099@science.zu.edu.eg>
2026-02-08 20:32:36 +0200

    Use execFile instead of exec | Filter results in JS instead of jq command pipeline

--- server.js
+++ server.js
@@ -2,43 +2,65 @@ const express = require("express");
 const app = express();
 const { exec, execFile } = require("node:child_process");
 const { filesize } = require("filesize");
+const ms = require("ms");
 
 app.get("/test", (req, res) => {
     res.json({ message: "Hello from the API!" });
 });
 
-function convertSizes(formats) {
-    return formats.map((format) => ({
-        ...format,
-        filesize: filesize(format.filesize),
-    }));
-}
-
 app.get("/api/video-sizes/:videoTag", (req, res) => {
     const startTime = Date.now();
     const videoTag = req.params.videoTag;
 
-    const command = `yt-dlp -J --no-warnings ${videoTag} | jq {'id: .id, title: .title, formats: [.formats[] | select(.height != null and .filesize != null) | {format_id, height, filesize}]}'`;
+    execFile(
+        "yt-dlp",
+        ["-J", "--no-warnings", videoTag],
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing yt-dlp: ${error.message}`);
+                return res
+                    .status(500)
+                    .json({ error: "Failed to retrieve video formats" });
+            }
+            const data = JSON.parse(stdout);
 
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing yt-dlp: ${error.message}`);
-            return res
-                .status(500)
-                .json({ error: "Failed to retrieve video formats" });
-        }
-        const endTime = Date.now();
-        const duration = `${endTime - startTime} ms`;
+            const endTime = Date.now();
 
-        const parsedData = JSON.parse(stdout);
+            const videoFormats = ["394", "395", "396", "397", "398", "399"];
+            const audioFormat = "251";
+            
+            const parsedData = {
+                id: data.id,
+                title: data.title,
+                duration: ms(data.formats[0].fragments[0].duration * 1000),
+                audioFormat: filesize(
+                    data.formats.filter(
+                        (format) => format.format_id === audioFormat,
+                    )[0].filesize_approx,
+                ),
+                videoFormats: data.formats
+                    .filter(
+                        (format) =>
+                            format.height &&
+                            format.filesize &&
+                            videoFormats.includes(format.format_id),
+                    )
+                    .map((format) => {
+                        return {
+                            format_id: format.format_id,
+                            height: format.height,
+                            filesize: filesize(format.filesize),
+                        };
+                    }),
+                executionTime: ms(endTime - startTime),
+            };
 
-        parsedData.duration = duration;
-        parsedData.formats = convertSizes(parsedData.formats);
-        
-        res.json(parsedData);
-    });
+            res.json(parsedData);
+        },
+    );
 });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
