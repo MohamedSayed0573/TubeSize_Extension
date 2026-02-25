b3773832e3925538378670a2d6dac96a09a6dcb9
Mohamed <20412023101099@science.zu.edu.eg>
2026-02-25 21:39:33 +0200

    rollback the last change

--- utils/cache.ts
+++ utils/cache.ts
@@ -3,7 +3,7 @@ import CONFIG from "../config/constants";
 import env from "./env";
 import { logger } from "./logger";
 
-const redis = new Redis({
+export const redis = new Redis({
     host: env.REDIS_HOST,
     port: env.REDIS_PORT,
     lazyConnect: env.REDIS_ENABLED || false,
