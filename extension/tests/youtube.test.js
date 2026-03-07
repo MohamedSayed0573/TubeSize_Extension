e34d759af040baf7fa78f48b0ed184051550418c
Mohamed <20412023101099@science.zu.edu.eg>
2026-03-07 18:41:40 +0200

    Remove the test assests to fix github languages

--- extension/tests/youtube.test.js
+++ extension/tests/youtube.test.js
@@ -1,10 +1,4 @@
 import { extractYtInitial } from "../youtube";
-import path, { dirname } from "path";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 describe("extractYtInitial", () => {
     test("should extract YtInitialPlayerResponse from html page", async () => {
@@ -13,15 +7,5 @@ describe("extractYtInitial", () => {
 
         // ensure the parser doesn’t throw when given real YouTube HTML
         expect(() => extractYtInitial(parsedHtml)).not.toThrow();
-    });
-
-    test("should return ytInitialPlayerResponse with the correct properties", () => {
-        // Note: youtubePage.html should NOT be formatted using prettier.
-        const htmlPage = readFileSync(path.join(__dirname, "assets", "youtubePage.html"), "utf-8");
-        const ytInitial = extractYtInitial(htmlPage);
-
-        expect(ytInitial).toHaveProperty("streamingData");
-        expect(ytInitial).toHaveProperty("videoDetails");
-        expect(ytInitial.streamingData).toHaveProperty("adaptiveFormats");
     });
 });
