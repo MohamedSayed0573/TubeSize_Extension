cf518febfafba05629b04f33d573cb2ef0e5a119
Mohamed <20412023101099@science.zu.edu.eg>
2026-03-17 07:15:50 +0200

    fix import aliases + fix jest config to be able to use import aliases

--- tubesize/src/main.tsx
+++ tubesize/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import Popup from "@/pages/popup.tsx";
-import ErrorBoundary from "@/components/errorBoundary.tsx";
-import ErrorPage from "@/pages/error.tsx";
+import Popup from "@pages/popup.tsx";
+import ErrorBoundary from "@components/errorBoundary.tsx";
+import ErrorPage from "@pages/error.tsx";
 
 const domRoot = document.getElementById("root") as HTMLElement;
 
--- tubesize/vite.config.ts
+++ tubesize/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import webExtension from "vite-plugin-web-extension";
+import path from "path";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@styles": path.resolve(__dirname, "src/styles"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@options": path.resolve(__dirname, "src/options"),
+            "@tests": path.resolve(__dirname, "src/tests"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@app-types": path.resolve(__dirname, "src/types"),
+        },
+    },
     plugins: [
         react({
             babel: {
