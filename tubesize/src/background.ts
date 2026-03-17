31851b8925b54ee03213877f300b99c4d9d64195
Mohamed <20412023101099@science.zu.edu.eg>
2026-03-18 00:30:14 +0200

    Refactor background.ts to improve readability

--- tubesize/src/background.ts
+++ tubesize/src/background.ts
@@ -23,86 +23,96 @@ chrome.runtime.onMessage.addListener(
         sender: chrome.runtime.MessageSender,
         sendResponse: (response: BackgroundResponse) => void,
     ) => {
-        if (message.type === "clearBadge") {
-            clearBadge(sender.tab?.id);
-            sendResponse({ success: true, data: null, cached: false });
-            return;
-        }
-        if (message.type === "setBadge") {
-            addBadge(sender.tab?.id);
-            sendResponse({ success: true, data: null, cached: false });
-            return;
-        }
-
-        if (message.type !== "sendYoutubeUrl") {
-            return;
-        }
-
-        const tag = message.tag;
-        const tabId = sender.tab?.id ?? message.tabId;
-
-        clearBadge(tabId);
-
-        (async () => {
-            const cached = await getFromStorage(tag);
-
-            if (cached) {
-                addBadge(tabId);
-                sendResponse({
-                    success: true,
-                    data: cached.response,
-                    cached: true,
-                    createdAt: cached.createdAt,
-                });
-                return;
-            }
-
-            try {
-                const html = message.html ?? (await fetchHTMLPage(tag));
-                const rawData = extractYtInitialForVideo(html, tag);
-                const rawFormats = parseDataFromYtInitial(rawData);
-                const humanizedFormats = humanizeData(rawFormats);
-
-                await saveToStorage(tag, humanizedFormats);
-                addBadge(tabId);
-                sendResponse({
-                    success: true,
-                    data: humanizedFormats,
-                    cached: false,
-                    api: false,
-                });
-            } catch (err: any) {
-                console.error("Couldn't use local, " + err);
-
-                try {
-                    const useAPIFallback = await getAPIFallbackSetting();
-                    if (!useAPIFallback) {
-                        throw new Error(err?.message || "An unknown error occurred");
-                    }
-
-                    const apiData = await fetchAPI(tag);
-                    // Do not cache API responses, in order to keep the cache consistent.
-                    // Because the API response is different than the data we extract from the html page.
-                    addBadge(tabId);
-                    sendResponse({
-                        success: true,
-                        data: apiData,
-                        cached: false,
-                        api: true,
-                    });
-                } catch (apiErr) {
-                    console.error(apiErr);
-                    clearBadge(tabId);
-                    sendResponse({
-                        success: false,
-                        data: null,
-                        cached: false,
-                        message: apiErr instanceof Error ? apiErr.message : "Unknown error",
-                    });
-                }
-            }
-        })();
-
-        return true; // Return true to keep the message channel open, because we have async operations
+        handleMessage(message, sender, sendResponse);
+        // Synchronously return true to indicate that sendResponse will be called asynchronously
+        return true;
     },
 );
+
+async function handleMessage(
+    message: Message,
+    sender: chrome.runtime.MessageSender,
+    sendResponse: (response: BackgroundResponse) => void,
+) {
+    const tabId = sender.tab?.id ?? message.tabId;
+
+    switch (message.type) {
+        case "clearBadge":
+            clearBadge(tabId);
+            return sendResponse({ success: true, data: null, cached: false });
+        case "setBadge":
+            addBadge(tabId);
+            return sendResponse({ success: true, data: null, cached: false });
+        case "sendYoutubeUrl":
+            return await handleMain(message, tabId, sendResponse);
+        default:
+            return;
+    }
+}
+
+async function handleMain(
+    message: Message,
+    tabId: number | undefined,
+    sendResponse: (response: BackgroundResponse) => void,
+) {
+    const { tag, html } = message;
+    clearBadge(tabId);
+
+    const cached = await getFromStorage(tag);
+    if (cached) {
+        addBadge(tabId);
+        return sendResponse({
+            success: true,
+            data: cached.response,
+            cached: true,
+            createdAt: cached.createdAt,
+        });
+    }
+
+    try {
+        const pageHtml = html ?? (await fetchHTMLPage(tag));
+        const rawData = extractYtInitialForVideo(pageHtml, tag);
+        const rawFormats = parseDataFromYtInitial(rawData);
+        const humanizedFormats = humanizeData(rawFormats);
+
+        await saveToStorage(tag, humanizedFormats);
+        addBadge(tabId);
+        return sendResponse({
+            success: true,
+            data: humanizedFormats,
+            cached: false,
+            api: false,
+        });
+    } catch (err) {
+        console.error("Couldn't use local, " + err);
+
+        try {
+            const useAPIFallback = await getAPIFallbackSetting();
+            if (!useAPIFallback) {
+                const errorMessage =
+                    err instanceof Error ? err.message : "An unknown error occurred";
+                throw new Error(errorMessage);
+            }
+
+            const apiData = await fetchAPI(tag);
+            // Do not cache API responses, in order to keep the cache consistent.
+            // Because the API response is different than the data we extract from the html page.
+            addBadge(tabId);
+            return sendResponse({
+                success: true,
+                data: apiData,
+                cached: false,
+                api: true,
+            });
+        } catch (apiErr) {
+            console.error(apiErr);
+            clearBadge(tabId);
+            return sendResponse({
+                success: false,
+                data: null,
+                cached: false,
+                message: apiErr instanceof Error ? apiErr.message : "Unknown error",
+            });
+        }
+    }
+}
