616ed55ec863f6602c4441089701353284b6a164
Mohamed <mohamedsaid0573@gmail.com>
2026-04-02 04:05:44 +0200

    Fix API env mounts and increase yt-dlp timeout

--- api/src/config/constants.ts
+++ api/src/config/constants.ts
@@ -23,7 +23,7 @@ export default {
     FALLBACK_VIDEO_FORMAT_IDS: ["278", "242", "243", "244", "247"],
     AUDIO_FORMAT_ID: "251",
     FALLBACK_AUDIO_FORMAT_IDS: "251-drc",
-    YTDLP_TIMEOUT_MS: 30000,
+    YTDLP_TIMEOUT_MS: 40000,
     WINDOW_LIMIT_MS: 15 * 60 * 1000, // 15 minutes / Time frame for which requests are checked/remembered
     LIMIT: 20, // Number of requests allowed in the time frame
     YOUTUBE_ID_REGEX: /^[a-zA-Z0-9_-]{11}$/,
